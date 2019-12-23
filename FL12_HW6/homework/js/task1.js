e0c876288eefa9f747cfa379a405663ca3c58cf1
Nataly Panakhyd <natalycoding@gmail.com>
2019-12-23 23:13:35 +0200

    task 1 quadratic equation

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,18 @@
-// Your code goes here
+let a = prompt('Enter value for A'),
+    b = prompt('Enter value for B'),
+    c = prompt('Enter value for C');
+if (isNaN(a || b || c) === true) {
+    console.log('Invalid input data');
+}
+let discr = (b * b) - 4 * (a * c);
+let sqrDiscr = Math.sqrt(discr);
+if (discr === 0) {
+    let x = -b / 2 * a;
+    console.log('x = ' + x);
+} else if (d > 0) {
+    let x1 = (-b + sqrDiscr) / (2 * a);
+    let x2 = (-b - sqrDiscr) / (2 * a);
+    console.log('x1 = ' + x1 + ' x2 = ' + x2);
+} else {
+    console.log('no solution');
+}
