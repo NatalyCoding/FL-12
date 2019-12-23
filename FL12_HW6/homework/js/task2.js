4a01da2c137051d0cf6aabc85b14c3354f260c92
Nataly Panakhyd <natalycoding@gmail.com>
2019-12-23 23:40:27 +0200

    task 2 triangle

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,19 @@
-// Your code goes here
+let a = parseInt(prompt('Enter first side of triangle - A')),
+    b = parseInt(prompt('Enter second side - B')),
+    c = parseInt(prompt('Enter third side - C'));
+if (isNaN(a) || isNaN(b) || isNaN(c) || a === '' || b === '' || c === '') {
+    alert('input values should be ONLY numbers');
+} else if (a === 0 || b === 0 || c === 0) {
+    alert('A triangle must have 3 sides with a positive definite length');
+} else {
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        alert('Triangle doesn’t exist');
+        console.log('Triangle doesn’t exist');
+    } else if (a === b && a === c) {
+        console.log('Equilateral triangle');
+    } else if (a === b || a === c || b === c) {
+        console.log('Isosceles triangle');
+    } else {
+        console.log('Scalene triangle');
+    }
+}
