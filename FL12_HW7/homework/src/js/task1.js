ef42995eadd40e0ffc3425ea1ea90c73ad3f7b83
Nataly Panakhyd <natalycoding@gmail.com>
2019-12-30 21:42:27 +0200

    refactored code (DRY)

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,28 +1,26 @@
 let email = prompt('Enter your email', '');
-let checkNumber = 5;
+const checkPasswLength = 5;
+let userEmail = 'user@gmail.com';
+let adminEmail = 'admin@gmail.com';
+let userPassw = 'UserPass';
+let adminPassw = 'AdminPass';
+let passw, changePassw, oldPassw, newPassw, confirmPassw;
 
 if (email === '' || email === null) {
     alert('Canceled');
-} else if (email.length < checkNumber) {
+} else
+if (email.length < checkPasswLength) {
     alert('I don\'t know any emails having name length less than 5 symbols');
-} else if (email === 'user@gmail.com') {
-    let pass1 = prompt('Enter your password', '');
-    if (pass1 === 'UserPass') {
+} else if (email === 'userEmail' || email === 'adminEmail') {
+    passw = prompt('Enter your password', '');
+    if (email === 'userEmail' && passw === 'userPassw' || email === 'adminEmail' && passw === 'adminPassw') {
         confirm('Do you want to change your password?', '');
-    } else if (pass1 === '' || pass1 === null) {
+    } else if (passw === '' || passw === null) {
         alert('Canceled');
     } else {
         alert('Wrong password');
     }
-} else if (email === 'admin@gmail.com') {
-    let pass2 = prompt('Enter your password', '');
-    if (pass2 === 'AdminPass') {
-        confirm('Do you want to change your password?', '');
-    } else if (pass2 === '' || pass2 === null) {
-        alert('Canceled');
-    } else {
-        alert('Wrong password');
-    }
+
 } else {
     alert('I don\'t know you');
 }
