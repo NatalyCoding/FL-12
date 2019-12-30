363cd7fe3881d2d8498dd18960c694244530bb8d
Nataly Panakhyd <natalycoding@gmail.com>
2019-12-30 22:28:49 +0200

    step1 basic game logic

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,1 +1,9 @@
-// Your code goes here
+let pocket;
+let pocketRandom;
+let randomRange = 9;
+let play = confirm('Do you want to play a game?');
+if (play) {
+    pocketRandom = Math.floor(Math.random() * randomRange);
+} else {
+    alert('You did not become a billionaire, but can.');
+}
