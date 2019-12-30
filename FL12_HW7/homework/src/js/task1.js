308145998db55fb09d269054f6f288e6686edcab
Nataly Panakhyd <natalycoding@gmail.com>
2019-12-30 12:27:23 +0200

    task1 check login

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,1 +1,11 @@
-// Your code goes here
+let email = prompt('Enter your email');
+
+if (email === '' || email === null) {
+    alert("Canceled");
+} else if (email.length < 5) {
+    alert("I don't know any emails having name length less than 5 symbols");
+} else if (email === 'user@gmail.com' || email === 'admin@gmail.com') {
+    prompt('Enter your password');
+} else {
+    alert("I don’t know you");
+}
