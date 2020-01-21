e03112d954d540d1d79e2c8e4b0a6d44bbadc300
Nataly Panakhyd <natalycoding@gmail.com>
2020-01-21 21:22:22 +0200

    create an object

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,1 +1,26 @@
-/* Your code goes here */
+function Fighter({ name, damage, strength, agility, health }) {
+
+    this.name = name;
+    this.damage = damage;
+    this.strength = strength;
+    this.agility = agility;
+    this.health = health;
+
+    let wins = 0;
+    let losses = 0;
+
+    return {
+
+        getName: () => name,
+        getDamage: () => damage,
+        getStrength: () => strength,
+        getAgility: () => agility,
+        getHealth: () => health,
+        addWin: () => wins++,
+        addLoss: () => losses++,
+
+        logCombatHistory: function() {
+            console.log(`Name: ${name}, Wins: ${wins}, Losses: ${losses}`);
+        }
+    }
+}
